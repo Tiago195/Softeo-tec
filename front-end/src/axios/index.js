15a47c2e90f4a178964d5fd47d5366d5d1253353
Tiago195 <tiagocf475@gmail.com>
2022-09-08 14:30:20 -0300

    Done

--- front-end/src/axios/index.js
+++ front-end/src/axios/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'https://softeo-api-123.herokuapp.com',
 });
 
 export default instance;
