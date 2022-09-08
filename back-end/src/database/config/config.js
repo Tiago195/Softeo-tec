8d0abe478c8e09a5dc8f52ab3a1e476134ab556e
Tiago195 <tiagocf475@gmail.com>
2022-09-08 14:02:39 -0300

    Done

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -6,7 +6,8 @@ module.exports = {
     'password': process.env.PASSWORD || null,
     'database': process.env.DATABASE || 'database_development',
     'host': process.env.HOST || '127.0.0.1',
-    'dialect': 'mysql'
+    'port': process.env.PORT_DB,
+    'dialect': 'postgres'
   },
   'test': {
     'username': 'root',
